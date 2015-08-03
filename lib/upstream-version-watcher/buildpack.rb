d5ede6657dc14296bc2660c54e89ce0aa6a077f6
Mike Dalessio <mike.dalessio@gmail.com>
2015-08-03 13:26:59 -0400

    add Buildpack class and specs

--- lib/upstream-version-watcher/buildpack.rb
+++ lib/upstream-version-watcher/buildpack.rb
@@ -1,5 +1,12 @@
 module UpstreamVersionWatcher
   class Buildpack
-    
+    attr_reader :url, :branch
+
+    DEFAULT_BRANCH = "master"
+
+    def initialize hash
+      raise ArgumentError.new("'url' is required") unless @url = hash["url"]
+      @branch = hash["branch"] || DEFAULT_BRANCH
+    end
   end
 end
--- spec/unit/buildpack_spec.rb
+++ spec/unit/buildpack_spec.rb
@@ -1,7 +1,23 @@
 require 'spec_helper'
 
 describe UpstreamVersionWatcher::Buildpack do
-  it "asserts a thing" do
-    expect(true).to eq false
+  Buildpack = UpstreamVersionWatcher::Buildpack
+
+  describe "initialization and defaults" do
+    it "accepts a hash of string keys" do
+      expect(Buildpack.new("url" => "https://foo.bar/bazz/quux").url).to eq "https://foo.bar/bazz/quux"
+    end
+
+    it "requires 'url'" do
+      expect { Buildpack.new({}) }.to raise_error(ArgumentError)
+    end
+
+    it "allows 'branch'" do
+      expect(Buildpack.new("url" => "https://foo.bar/bazz/quux", "branch" => "develop").branch).to eq "develop"
+    end
+
+    it "defaults 'branch' to 'master'" do
+      expect(Buildpack.new("url" => "https://foo.bar/bazz/quux").branch).to eq "master"
+    end
   end
 end
